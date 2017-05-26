13a6a1420898a3daf724dce2e04c2246a6aea71f
Victor Allen <vamuigua@gmail.com>
2017-05-26 16:09:19 +0300

    Modified name output

--- js/search.js
+++ js/search.js
@@ -1,22 +1,33 @@
 var apiKey = require('./../.env').apiKey;
 
 //Call-back function after gitlook function
-function output(){
+function output() {
     console.log("SUCEESS!");
 }
 
 //Gitlook Function
-function gitlook(){
-}
+function gitlook() {}
 
-//function to make an authenticated API call
+//function to make an authenticated API call for username
+gitlook.prototype.getRepos = function (username) {
+    $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function (response) {
+        $('#result-name').text("Username:"+response.name);
+        console.log(response);
+    }).fail(function (error) {
+        console.log(error.responseJSON.message);
+    });
+};
+
+/*
+//function to make an authenticated API call for Repos
 gitlook.prototype.getRepos = function(username){
-  $.get('https://api.github.com/users/'+username+'?access_token=' + apiKey).then(function(response){
+$.get('https://api.github.com/users/'+username+'/repos'+'?access_token=' + apiKey).then(function(response){
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
+*/
 
 //Export modules
 exports.gitlookModule = gitlook;
