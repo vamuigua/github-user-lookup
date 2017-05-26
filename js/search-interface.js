5530dd08f6469945c965a4ccd7d62cfdbcb61b2f
Victor Allen <vamuigua@gmail.com>
2017-05-26 18:25:37 +0300

    Modified content

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,12 +1,31 @@
 var apiKey = require('./../.env').apiKey;
 var gitlook = require('./../js/search.js').gitlookModule;
+var output = require('./../js/search.js').outputModule;
+
+/*
+//Display Username
+function displayName(username) {
+    if (username === null) {
+        $('#result-name').text("The users has no name in profile");
+    } else {
+        $('#result-name').text(username);
+    }
+}
+*/
+
+//Display Repo-Details
+function displayRepos(repoName, repoDescription) {
+    $('#repos').append("<li><h4>" + repoName + "</h4><br>" + repoDescription + "<br>");
+}
 
 //Main run function
 $(document).ready(function () {
     $('#gitlook-form').submit(function (event) {
         var gitlookObject = new gitlook();
+        var outputObject = new output();
         var username = $('#username-input').val();
-        gitlookObject.getRepos(username);
+        gitlookObject.getUsername(username);
+        outputObject.getRepos(username, displayRepos);
         event.preventDefault();
     });
 });
--- js/search.js
+++ js/search.js
@@ -1,33 +1,40 @@
 var apiKey = require('./../.env').apiKey;
 
 //Call-back function after gitlook function
-function output() {
-    console.log("SUCEESS!");
-}
+function output() {}
 
 //Gitlook Function
 function gitlook() {}
 
 //function to make an authenticated API call for username
-gitlook.prototype.getRepos = function (username) {
+gitlook.prototype.getUsername = function (username) {
     $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function (response) {
-        $('#result-name').text("Username:"+response.name);
-        console.log(response);
+        console.log(response.name);
+        $('#result-name').text("Username:" + response.name);
+        //displayName(response.name);
     }).fail(function (error) {
         console.log(error.responseJSON.message);
     });
 };
 
-/*
+
 //function to make an authenticated API call for Repos
-gitlook.prototype.getRepos = function(username){
-$.get('https://api.github.com/users/'+username+'/repos'+'?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
+output.prototype.getRepos = function (username, displayRepos) {
+    $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function (response) {
+        for (var i = 0; i < response.length + 1; i++) {
+            if (response[i].description === null) {
+                response[i].description = 'No description in repo';
+            } else {
+                displayRepos(response[i].name, response[i].description);
+            }
+
+        }
+    }).fail(function (error) {
+        console.log(error.responseJSON.message);
+    });
 };
-*/
+
 
 //Export modules
 exports.gitlookModule = gitlook;
+exports.outputModule = output;
