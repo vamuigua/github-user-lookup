6b2f2fa3e2e578e2600d69df7d8ec77eee865cf0
Victor Allen <vamuigua@gmail.com>
2017-06-21 18:06:04 +0300

    Modified to display details of the username and repos

--- js/search-interface.js
+++ js/search-interface.js
@@ -2,30 +2,20 @@ var apiKey = require('./../.env').apiKey;
 var gitlook = require('./../js/search.js').gitlookModule;
 var output = require('./../js/search.js').outputModule;
 
-/*
-//Display Username
-function displayName(username) {
-    if (username === null) {
-        $('#result-name').text("The users has no name in profile");
-    } else {
-        $('#result-name').text(username);
-    }
-}
-*/
-
 //Display Repo-Details
 function displayRepos(repoName, repoDescription) {
-    $('#repos').append("<li><h4>" + repoName + "</h4><br>" + repoDescription + "<br>");
+  $('#repos').append("<li><h4>" + repoName + "</h4><br>" + repoDescription + "<hr><br>");
 }
 
 //Main run function
-$(document).ready(function () {
-    $('#gitlook-form').submit(function (event) {
-        var gitlookObject = new gitlook();
-        var outputObject = new output();
-        var username = $('#username-input').val();
-        gitlookObject.getUsername(username);
-        outputObject.getRepos(username, displayRepos);
-        event.preventDefault();
-    });
+$(document).ready(function() {
+  $('#gitlook-form').submit(function(event) {
+    var gitlookObject = new gitlook();
+    var outputObject = new output();
+    var username = $('#username-input').val();
+    $('#username-input').val("");
+    gitlookObject.getUsername(username);
+    outputObject.getRepos(username, displayRepos);
+    event.preventDefault();
+  });
 });
